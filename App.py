8694b0cc0f3f35944fb975f675dd0b0a61cbc556
Yan <141505117+lukianchik@users.noreply.github.com>
2024-08-24 20:59:56 +0300

    Update App.py

--- App.py
+++ App.py
@@ -27,53 +27,49 @@ def process_and_plot_data(model, file_path, root):
         if 'Weekly_Sales' in df_test.columns.values.tolist():
             df_test = df_test.drop(columns=['Weekly_Sales'])
             name_of_graph = 'Прошлые продажи, $'
-            predictions = df_test.Weekly_Sales
-            df_predictions = pd.DataFrame({'Date': df_test['Date'], 'Sales': predictions})
-            df_grouped = df_test.groupby('Date', as_index=False).sum()
-            display_graph(df_grouped, root, name_of_graph)
         else:
             name_of_graph = 'Прогноз будущих продаж, $'
-            df_test["IsHoliday"] = df_test["IsHoliday"].astype(int)
-    
-            df_test['Date'] = pd.to_datetime(df_test['Date'])
-            df_test = df_test.sort_values(by='Date', ascending=True)
-    
-            df_date = df_test.copy()
-            df_test['day'] = df_test['Date'].dt.day
-            df_test["day"] = df_test["day"].astype(int)
-    
-            df_test['month'] = df_test['Date'].dt.month
-            df_test["month"] = df_test["month"].astype(int)
-    
-            df_test['year'] = df_test['Date'].dt.year
-            df_test["year"] = df_test["year"].astype(int)
-    
-            df_test['week'] = df_test['Date'].dt.isocalendar().week
-            df_test["week"] = df_test["week"].astype(int)
-    
-            df_test = df_test.drop(columns=['Date'])
-    
-            df_test['Discount'] = df_test['MarkDown1'] + df_test['MarkDown2'] + df_test['MarkDown3'] + df_test['MarkDown4'] + df_test['MarkDown5']
-            df_test = df_test.drop(columns=['MarkDown1','MarkDown2','MarkDown3', 'MarkDown4', 'MarkDown5'])
-    
-            median_value = df_test['CPI'].median()
-            df_test.fillna({'CPI': median_value}, inplace=True)
-    
-            median_value = df_test['Unemployment'].median()
-            df_test.fillna({'Unemployment': median_value}, inplace=True)
-    
-            df_test.fillna(0, inplace=True)
-    
-            le = LabelEncoder()
-            df_test['Type'] = le.fit_transform(df_test['Type'])
-    
-            df_test = df_test.drop(columns=['Type', 'Unemployment', 'CPI'])
-            predictions = model.predict(df_test)
-    
-            df_predictions = pd.DataFrame({'Date': df_date['Date'], 'Sales': predictions})
-            df_grouped = df_predictions.groupby('Date', as_index=False).sum()
-    
-            display_graph(df_grouped, root, name_of_graph)
+        df_test["IsHoliday"] = df_test["IsHoliday"].astype(int)
+
+        df_test['Date'] = pd.to_datetime(df_test['Date'])
+        df_test = df_test.sort_values(by='Date', ascending=True)
+
+        df_date = df_test.copy()
+        df_test['day'] = df_test['Date'].dt.day
+        df_test["day"] = df_test["day"].astype(int)
+
+        df_test['month'] = df_test['Date'].dt.month
+        df_test["month"] = df_test["month"].astype(int)
+
+        df_test['year'] = df_test['Date'].dt.year
+        df_test["year"] = df_test["year"].astype(int)
+
+        df_test['week'] = df_test['Date'].dt.isocalendar().week
+        df_test["week"] = df_test["week"].astype(int)
+
+        df_test = df_test.drop(columns=['Date'])
+
+        df_test['Discount'] = df_test['MarkDown1'] + df_test['MarkDown2'] + df_test['MarkDown3'] + df_test['MarkDown4'] + df_test['MarkDown5']
+        df_test = df_test.drop(columns=['MarkDown1','MarkDown2','MarkDown3', 'MarkDown4', 'MarkDown5'])
+
+        median_value = df_test['CPI'].median()
+        df_test.fillna({'CPI': median_value}, inplace=True)
+
+        median_value = df_test['Unemployment'].median()
+        df_test.fillna({'Unemployment': median_value}, inplace=True)
+
+        df_test.fillna(0, inplace=True)
+
+        le = LabelEncoder()
+        df_test['Type'] = le.fit_transform(df_test['Type'])
+
+        df_test = df_test.drop(columns=['Type', 'Unemployment', 'CPI'])
+        predictions = model.predict(df_test)
+
+        df_predictions = pd.DataFrame({'Date': df_date['Date'], 'Sales': predictions})
+        df_grouped = df_predictions.groupby('Date', as_index=False).sum()
+
+        display_graph(df_grouped, root, name_of_graph)
         
     except Exception as e:
         messagebox.showerror("Ошибка", f"Не удалось обработать данные: {e}")
